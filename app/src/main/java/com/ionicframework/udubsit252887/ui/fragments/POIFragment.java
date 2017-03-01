37058bbecd207baa5aa61f1bbb600fb2edab4dbe
BrentVanwildem <brent.vanwildemeersch@hotmail.be>
2017-03-01 19:37:24 +0200

    First Try

--- app/src/main/java/com/ionicframework/udubsit252887/ui/fragments/POIFragment.java
+++ app/src/main/java/com/ionicframework/udubsit252887/ui/fragments/POIFragment.java
@@ -26,9 +26,13 @@ import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.ionicframework.udubsit252887.R;
+import com.ionicframework.udubsit252887.Utils.Constants;
 import com.ionicframework.udubsit252887.models.Poi;
 import com.ionicframework.udubsit252887.ui.activities.MainActivity;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -41,7 +45,8 @@ public class POIFragment extends Fragment {
     private View rootView;
     private ListView mPOIListView;
     private FirebaseListAdapter<Poi> mPOIAdapter;
-
+    private String limitQueryStr;
+    private Query databaseRef;
     public POIFragment() {
         // Required empty public constructor
     }
@@ -64,11 +69,14 @@ public class POIFragment extends Fragment {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
+                limitQueryStr = s;
+                CustomPOIAdapter();
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String s) {
+
                 return false;
             }
         });
@@ -81,11 +89,56 @@ public class POIFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_poi, container, false);
         mPOIListView = (ListView) rootView.findViewById(R.id.poi_listview);
 
+        CustomPOIAdapter();
+//        mPOIAdapter = new FirebaseListAdapter<Poi>(
+//                getActivity(),
+//                Poi.class,
+//                R.layout.item_layout_poi,
+//                FirebaseDatabase.getInstance().getReference("poi").orderByChild("name")) {
+//            @Override
+//            protected void populateView(View v, Poi model, int position) {
+//                TextView pointsText = (TextView) v.findViewById(R.id.poi_textview);
+//                pointsText.setText(model.getName());
+//                TextView pointDescr = (TextView) v.findViewById(R.id.poi_building);
+//                pointDescr.setText(model.getDescription());
+//            }
+//        };
+//
+//        mPOIListView.setAdapter(mPOIAdapter);
+//
+//        mPOIListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Poi poi = mPOIAdapter.getItem(position);
+//
+//                Uri gmmIntentUri = Uri.parse("geo:0,0?q=" + poi.getLatitude() + "," + poi.getLongitude() + "(" + poi.getDescription() + ")=w");
+//                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+//                mapIntent.setPackage("com.google.android.apps.maps");
+//                startActivity(mapIntent);
+//            }
+//        });
+        return rootView;
+    }
+
+    private void CustomPOIAdapter(){
+        if(limitQueryStr !=null){
+            //input query is != empty
+            databaseRef = FirebaseDatabase.getInstance().getReference(Constants.POI_KEY)
+                    .orderByChild("name")
+                    .startAt(limitQueryStr)
+                    .endAt(limitQueryStr +"~");
+
+        }
+        else {
+            databaseRef = FirebaseDatabase.getInstance().getReference(Constants.POI_KEY).orderByChild("name");
+
+        }
+
         mPOIAdapter = new FirebaseListAdapter<Poi>(
                 getActivity(),
                 Poi.class,
                 R.layout.item_layout_poi,
-                FirebaseDatabase.getInstance().getReference("poi").orderByChild("name")) {
+                databaseRef){
             @Override
             protected void populateView(View v, Poi model, int position) {
                 TextView pointsText = (TextView) v.findViewById(R.id.poi_textview);
@@ -96,6 +149,8 @@ public class POIFragment extends Fragment {
         };
 
         mPOIListView.setAdapter(mPOIAdapter);
+
+       Integer aItems= mPOIListView.getAdapter().getCount();
 
         mPOIListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -108,7 +163,8 @@ public class POIFragment extends Fragment {
                 startActivity(mapIntent);
             }
         });
-        return rootView;
+
     }
+
 
 }
