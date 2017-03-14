b7e818eb840104fe41ab41a5d432174a013a1823
BrentVanwildem <brent.vanwildemeersch@hotmail.be>
2017-03-14 10:16:08 +0200

    Splash screens

---
+++ app/src/main/java/com/ionicframework/udubsit252887/ui/activities/SplashActivity.java
@@ -0,0 +1,11 @@
+package com.ionicframework.udubsit252887.ui.activities;
+
+import android.app.Activity;
+
+/**
+ * Created by Test on 14/03/2017.
+ */
+
+public class SplashActivity extends Activity {
+
+}
